b5ab98874e77a7086ca366c0ced3e4363ae99838
Keenan Weise <kcweise@gmail.com>
2024-03-19 15:02:47 -0500

    All plants render upon refresh

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,9 +1,20 @@
 import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
+function PlantList({list}) {
+  
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {list.map((plant)=>(
+      <PlantCard
+      key = {plant.id}
+      id = {plant.id}
+      name ={plant.name}
+      image = {plant.image}
+      price = {plant.price}/>
+      ))}
+      
+    </ul>
   );
 }
 
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+const[list, setList] = useState([])
+const url = 'http://localhost:6001/plants'
+
+useEffect(()=> {
+  fetch(url)
+    .then (res => {
+      if (!res.ok) {
+        console.error("Error fetching data");
+      }
+      return res.json();       
+    })
+    .then (data => setList(data))
+    .catch(error => console.error("Error fetching data:", error));   
+}, []);
+
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList list = {list} />
     </main>
   );
 }
