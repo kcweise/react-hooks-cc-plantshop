66ddf8b1624c9973fa019b79f3d4a0f7833d6a95
Keenan Weise <kcweise@gmail.com>
2024-03-20 08:40:57 -0500

    Added click on price, edit, PATCH to database functionality

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,66 @@
 import React, {useState} from "react";
 
-function PlantCard({ name, image, price }) {
-
+//price is renamed to initialPrice
+function PlantCard({ id, name, image, price: initialPrice }) {
  const[stock, setStock] = useState(true);
+ const[isEditingPrice, setIsEditingPrice]= useState(false);
+ const[price, setPrice] = useState(initialPrice);
 
  const toggleStock = () => setStock(!stock)
+
+ //sets input values to be displayed
+ const handlePriceChange = (e) => {
+  setPrice(e.target.value);
+ }
+
+ //toggles the price display to an input field
+ const handlePriceClick=()=>{
+  setIsEditingPrice(true);
+ }
+//Upon clicking out of the input field sets price and POSTs to database
+ const handlePriceBlur=()=> {
+  setIsEditingPrice(false);
+  updatePrice(id, price);
+ }
+
+const updatePrice =(plantId, newPrice) => {
+  const url = `http://localhost:6001/plants/${plantId}`;
+
+  fetch(url,{
+    method:'PATCH',
+    headers: {'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      price: newPrice,
+    }),
+  })
+  .then(res => {
+    if(!res.ok) {
+      console.log("Error with Fetch")
+    }
+    return res.json();
+  })
+  .then(updatedPlant => {
+    console.log("Price update was successful", updatedPlant);
+  })
+  .catch (error =>{
+    console.log('Error updating price', error);
+  });
+};
+
   
   return (
     <li className="card" data-testid="plant-item">
       <img src={image} alt={name} />
       <h4>{name}</h4>
-      <p>Price: {price}</p>
+      <p onClick = {handlePriceClick}>Price: {isEditingPrice?(
+        <input 
+        type="number"
+        value = {price}
+        onChange={handlePriceChange}
+        onBlur={handlePriceBlur}
+        autoFocus/>):(price)}
+      </p>
       {stock ? (
         <button className="primary" onClick={toggleStock}>In Stock</button>
       ) : (
